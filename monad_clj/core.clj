1ccebd209d8896824a59b708683cb8cf9f18be1b
Shulang Lei <shulang.lei@unbounce.com>
2017-06-30 17:31:04 -0700

    Probability distribution monad example added

--- monad_clj/core.clj
+++ monad_clj/core.clj
@@ -128,6 +128,73 @@
               b (m-result (inc a))]
              b))
 
+;;the maybe monad transformer
+(defn maybe-t
+  [m]
+  (m/monad [m-result (m/with-monad m m-result)
+            m-bind (m/with-monad m
+                     (fn [mv f]
+                       (m-bind mv
+                               (fn [x]
+                                 (if (nil? x)
+                                   (m-result nil)
+                                   (f x))))))
+            m-zero (m/with-monad m m-zero)
+            m-plus (m/with-monad m m-plus)
+            ]))
+
+(def maybe-in-seq-M (maybe-t sequence-m))
+
+(m/with-monad maybe-in-seq-M
+  (m/domonad [a [1 2 3 nil]
+              b (m-result (inc a))]
+             b))
+
+(m/with-monad maybe-in-seq-M
+  (m/domonad [a [0 nil 1 2 3]
+              b [1 2 3 4 5]]
+             (* a b)))
+;;notice that nil is only appear once. Because (* a b) is ommited.  Whether the last m-result is executed depends on the m-bind, in this case, when a is a nil, the b binding function will not be executed - line:140
+
+;; probability distribution monad
+(m/defmonad dist-m
+  [m-result (fn [v] {v 1})
+   m-bind   (fn [mv f]
+              (letfn [(add-prob [dist [x p]]
+                        (assoc dist x (+ (get dist x 0) p)))]
+                (reduce add-prob {}
+                        (for [[x p] mv
+                              [y q] (f x)]
+                          [y (* q p)]))))])
+
+;; monty halls problem
+(defn uniform [s]
+  (apply array-map
+         (interleave s (repeat (/ 1 (count s))))))
+
+(def doors #{:a :b :c})
+
+;; picked a door
+(m/domonad dist-m
+           [prize (uniform doors)
+            choice (uniform doors)]
+           (= choice prize))
+
+;; revealed a door of no car
+(m/domonad dist-m
+           [prize (uniform doors)
+            choice (uniform doors)
+            reveal (uniform (disj doors prize choice))]
+           (= prize reveal))
+
+;; picked another door
+(m/domonad dist-m
+           [prize (uniform doors)
+            choice (uniform doors)
+            reveal (uniform (disj doors prize choice))
+            switch (uniform (disj doors choice reveal))]
+           (= switch prize))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
